881c63fdd23ea774dbd0f64afb1c217ef68201cf
jeppeman <jesperaamann@gmail.com>
2017-08-06 12:28:44 +0200

    Don't serialize compiler generated fields by default

---
+++ JsonFx/JsonFx.Json/SerializeCompilerGenerated.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JsonFx.Json
+{
+    /// <summary>
+    /// Attribute to specify that compiler generated fields should be serialized 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SerializeCompilerGenerated : Attribute
+    {
+        
+    }
+}
